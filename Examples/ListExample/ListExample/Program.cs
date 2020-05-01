def660c5d7343035e7560c2210fcce371b7f1c3a
Steffen Rahbek Vutborg <svu@ucn.dk>
2020-05-01 11:48:26 +0200

    Added List example

---
+++ Examples/ListExample/ListExample/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>();
+
+            int choise;
+            bool running = true;
+            while(running)
+            {
+                choise = printMenu();
+                Console.WriteLine(choise);
+                switch(choise)
+                {
+                    case 1:
+                        AddUser(users);
+                        break;
+                    case 2:
+                        RemoveUser(users);
+                        break;
+                    case 3:
+                        PrintUsers(users);
+                        break;
+                    case 4:
+                    default:
+                        running = false;
+                        break;
+                }              
+            }
+            Console.Read();
+        }
+
+        static int printMenu()
+        {
+            Console.WriteLine("Choose option");
+            Console.WriteLine("1: Add user");
+            Console.WriteLine("2: Remove user");
+            Console.WriteLine("3: Print users");
+            Console.WriteLine("4: Exit");
+
+            int result = -1;
+            int.TryParse(Console.ReadLine(), out result);
+            return result;
+        }
+
+        static void PrintUsers(List<User> users)
+        {
+            Console.WriteLine("=========Users=========");
+            foreach(User u in users)
+            {
+                Console.WriteLine(u);
+            }
+            Console.WriteLine("=====End of users======");
+        }
+
+        static void AddUser(List<User> users)
+        {
+            // generate unique ID
+            int iD = 1;
+            while(users.Exists(u => u.ID == iD))
+            {
+                iD++;
+            }
+            User newUser = new User("", "", iD);
+            
+            Console.WriteLine("Enter new user:");
+            Console.Write("Enter first name: ");
+            newUser.Name = Console.ReadLine();
+            Console.Write("Enter surname: ");
+            newUser.Surname = Console.ReadLine();
+
+            users.Add(newUser);
+        }
+        static void RemoveUser(List<User> users)
+        {
+            Console.WriteLine("Enter the ID of the user to be removed:");
+            int choise;
+            if (int.TryParse(Console.ReadLine(), out choise))
+            {
+                User u = users.Find(x => x.ID == choise);
+                Console.WriteLine($"Removing user: {u}");
+                users.Remove(u);
+            }
+        }
+    }
+}
---
+++ Examples/ListExample/ListExample/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExample
+{
+    class User
+    {
+        string name;
+        string surname;
+        int iD;
+
+        public User(string name, string surname, int id)
+        {
+            Name = name;
+            Surname = surname;
+            ID = id;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Surname { get => surname; set => surname = value; }
+        public int ID { get => iD; set => iD = value; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} {Surname}     ID: {ID}";
+        }
+    }
+}
